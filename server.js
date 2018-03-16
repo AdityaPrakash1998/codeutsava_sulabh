ac9af1243537b71bc45c5334c11cad23464cc2b9
Aditya Prakash <abekh19@gmail.com>
2018-03-16 13:19:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -64,4 +64,6 @@ app.get('/',function(req,res)
 let reviewRoute=require('./routes/review');
 app.use('/review',reviewRoute);
 
-app.listen('8080',()=>console.log('Server started at port 8080 !'));
+var port=process.env.PORT || 8080;
+
+app.listen(port,()=>console.log('Server started at port '+port));
